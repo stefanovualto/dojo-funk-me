5f1f9488ca603756bd70fb066608d830028c9637
stefanovualto <stefano.nepa@vualto.com>
2019-02-11 13:59:02 +0000

    remove old code

--- 3-pipe/1-base.js
+++ 3-pipe/1-base.js
@@ -1,20 +1,15 @@
 "use strict"
 // Base data
-const array = [1, 2, 3, 4];
-
-const incement = n => n + 1;
-const double = n => n * 2;
-
-const h = pipe(
-  incemrent,
-  traceAfterG,
-  double,
-  traceAfterF,
-);
-
 const h = (element) => {
     element = element + 1;
     console.log(`after increment: ${ element }`);
     element = element * 2;
     console.log(`after double: ${ element }`);
 };
+
+h(20);
+
+/*
+after increment: 21
+after double: 42
+*/
