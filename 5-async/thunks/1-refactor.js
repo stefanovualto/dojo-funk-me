c710350feb5c88e15cfb88120311638687fca14c
stefanovualto <stefano.nepa@vualto.com>
2019-02-14 13:07:58 +0000

    addd usage

--- 5-async/thunks/1-refactor.js
+++ 5-async/thunks/1-refactor.js
@@ -14,3 +14,20 @@ function thunk (param) {
         else { fn = cb; }
     }
 }
+
+// Usage
+
+let t1 = thunk("titi1");
+let t2 = thunk("titi2");
+let t3 = thunk("titi3");
+
+t1(function (t1Result) {
+    output(t1Result);
+    t2(function (t2Result) {
+        output(t2Result);
+        t3(function (t3Result) {
+            output(t3Result);
+            output("That's it!!")
+        })
+    })
+})
